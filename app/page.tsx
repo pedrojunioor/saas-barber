1e33e358d3d3c1a86119314840d233b805726f8a
Pedro <pedro.junior@infarma.com.br>
2026-03-15 10:50:59 -0300

    feat: add header and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-export default function Home() {
+import Image from "next/image"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { SearchIcon } from "lucide-react"
+import { Button } from "./_components/ui/button"
+const Home = async () => {
   return (
-    <div className="">
-      <h1 className="mb-12 p-4 text-red-300">Welcome to the Home Page</h1>
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-37.5 w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Home
